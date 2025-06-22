2ef9b7718aba511a25be863cd9a537c240e4c358
ayobanw <carpkingrc1@gmail.com>
2025-06-22 08:48:31 +0800

    Add files via upload

---
+++ OPEN CUSTOM SPEC 🅰🅽🆆 ®©™ V4.8.22.3.lua
@@ -0,0 +1,199 @@
+if os.date("%Y%m%d") > "20291110" then
+time=gg.alert([[
+                     ☣️THIS SCRIPT HAS EXPIRED ☣️
+🔸PLEASE REFER TO THE DEVELOPER TO RENEW YOUR SCRIPT🔸
+       🔹 TELEGRAM: ayobcicaknakal  🔹TIKTOK: SB_KP
+
+                         ╭══•  ೋ•✧๑♡๑✧•ೋ  •══╮
+
+                            𝑺𝑪𝑹𝑰𝑷𝑻 𝑩𝒀 𝑨𝒀𝑶𝑩 𝑷𝑹𝑶𝑮𝑹𝑨𝑴
+                                    ☣️🅰🅽🆆 ®©™☣️                               
+
+                         ╰══•  ೋ•✧๑♡๑✧•ೋ  •══╯]])
+os.exit()
+end
+
+print("𝑺𝑪𝑹𝑰𝑷𝑻 𝑩𝒀 𝑨𝒀𝑶𝑩 𝑷𝑹𝑶𝑮𝑹𝑨𝑴")
+
+gg.setVisible (false)
+gg.sleep(300)                             
+gg.sleep(300)
+gg.alert([[
+         ╭══•             ೋ•✧๑♡๑✧•ೋ.            •══╮
+         
+           𝑾𝑬𝑳𝑪𝑶𝑴 𝑻𝑶 𝑺𝑪𝑹𝑰𝑷𝑻 𝑩𝒀 𝑨𝒀𝑶𝑩 𝑷𝑹𝑶𝑮𝑹𝑨𝑴
+                                ☣️ 🅰🅽🆆 ®©™ ☣️
+                                          
+         ╰══•             ೋ•✧๑♡๑✧•ೋ.            •══╯]])
+gg.sleep(300)
+gg.toast("💫SABAR JAP")
+gg.sleep(300)
+gg.toast("💎𝟭𝟬%")
+gg.sleep(300)
+gg.toast("💎💎𝟮𝟬%")
+gg.sleep(300)
+gg.toast("💎💎💎𝟯𝟬%")
+gg.sleep(300)
+gg.toast("💎💎💎💎𝟰𝟬%")
+gg.sleep(300)
+gg.toast("💎💎💎💎💎𝟲𝟬%")
+gg.sleep(300)
+gg.toast("💎💎💎💎💎💎𝟲𝟱%")
+gg.sleep(300)
+gg.toast("💎💎💎💎💎💎💎𝟴𝟬%")
+gg.sleep(300)
+gg.toast("💎💎💎💎💎💎💎💎𝟴𝟱%")
+gg.sleep(300)
+gg.toast("💎💎💎💎💎💎💎💎💎𝟵𝟬%")
+gg.sleep(300)
+gg.toast("💎💎💎💎💎💎💎💎💎💎𝟭𝟬𝟬%")
+gg.sleep(300)
+gg.toast("SELESAI")
+gg.sleep(1000)
+gg.alert("SILA TEKAN LOGO GG")
+
+function start()
+menu = gg.choice({
+
+	"『🛠️』CUSTOM SPEC",
+	"『🛠️』CUSTOM SPEC (V3.5)",
+
+	"🦋𝙴𝚇𝙸𝚃🦋",
+	
+},nil,"✎✐Created 𝑩𝒀 𝑨𝒀𝑶𝑩 𝑷𝑹𝑶𝑮𝑹𝑨𝑴")
+
+if menu == 1 then
+gg.alert([[ BELI ENJIN YANG INGIN ANDA PAKAI
+           DAN BELI SEMUA OTHER ]])
+gg.clearResults()
+gg.clearList()
+-- main code on
+d = gg.prompt({
+'☣️Input ORI HP',
+'☣️Input EDIT HP',
+'☣️Input ORI TORQUE',
+'☣️Input EDIT TORQUE',
+'☣️Input ORI Inner HP',
+'☣️Input EDIT Inner HP',
+'☣️Input ORI inner TORQUE',
+'☣️Input EDIT inner TORQUE',
+'☣️Input SHIFT TIME'
+
+,},nil,{[1]=""},{[2]=""},{[3]=""},{[4]=""},{[5]=""},{[6]=""},{[7]=""},{[8]=""},{[9]=""})if d == nil then Menu() end gg.clearResults() gg.setVisible(false) 
+gg.setRanges(gg.REGION_ANONYMOUS) 
+gg.searchNumber(d[1],gg.TYPE_FLOAT) 
+revert = gg.getResults(100) 
+gg.editAll(d[2],gg.TYPE_FLOAT) 
+gg.clearResults() 
+gg.sleep(500) 
+gg.setVisible(false) 	
+gg.setRanges(gg.REGION_ANONYMOUS) 
+gg.searchNumber(d[3],gg.TYPE_FLOAT) 
+revert = gg.getResults(100) 
+gg.editAll(d[4],gg.TYPE_FLOAT) 
+gg.clearResults() 
+gg.sleep(500) 
+gg.setVisible(false) 	
+gg.setRanges(gg.REGION_ANONYMOUS) 
+gg.searchNumber(d[5],gg.TYPE_FLOAT) 
+revert = gg.getResults(100) 
+gg.editAll(d[6],gg.TYPE_FLOAT) 
+gg.clearResults() 
+gg.sleep(500) 
+gg.setVisible(false) 	
+gg.setRanges(gg.REGION_ANONYMOUS) 
+gg.searchNumber(d[7],gg.TYPE_FLOAT) 
+revert = gg.getResults(100) 
+gg.editAll(d[8],gg.TYPE_FLOAT) 
+gg.clearResults() 
+gg.sleep(500) 
+gg.setVisible(false)
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("0.1", gg.TYPE_FLOAT)
+revert = gg.getResults(100)
+gg.editAll(d[9],gg.TYPE_FLOAT)
+gg.clearResults() 
+gg.sleep(500)
+gg.setVisible(false)
+gg.processResume()
+gg.clearResults()
+gg.alert("BELI SEMULA ECU TUNING DAN FAST GEARBOX")
+end
+
+if menu == 2 then
+gg.alert([[ BELI ENJIN V3.5 DAN SEMUA OTHER  MACAM BIASA.
+         SEBELUM EDIT SPEC ]])
+gg.clearResults()
+gg.clearList()
+-- main code on
+d = gg.prompt({
+'☣️Input HP',
+'☣️Input TORQUE',
+'☣️Input Inner HP',
+'☣️Input inner TORQUE',
+'☣️Input SHIFT TIME'
+
+,},nil,{[1]=""},{[2]=""},{[3]=""},{[4]=""},{[5]=""},{[6]=""})if d == nil then Menu() end gg.clearResults() gg.setVisible(false) 
+gg.setRanges(gg.REGION_ANONYMOUS) 
+gg.searchNumber('423',gg.TYPE_FLOAT)
+revert = gg.getResults(100) 
+gg.editAll(d[1],gg.TYPE_FLOAT) 
+gg.clearResults() 
+gg.sleep(500) 
+gg.setVisible(false) 	
+gg.setRanges(gg.REGION_ANONYMOUS) 
+gg.searchNumber('630',gg.TYPE_FLOAT) 
+revert = gg.getResults(100) 
+gg.editAll(d[2],gg.TYPE_FLOAT) 
+gg.clearResults() 
+gg.sleep(500) 
+gg.setVisible(false) 	
+gg.setRanges(gg.REGION_ANONYMOUS) 
+gg.searchNumber('6300',gg.TYPE_FLOAT) 
+revert = gg.getResults(100) 
+gg.editAll(d[3],gg.TYPE_FLOAT) 
+gg.clearResults() 
+gg.sleep(500) 
+gg.setVisible(false) 	
+gg.setRanges(gg.REGION_ANONYMOUS) 
+gg.searchNumber('4700',gg.TYPE_FLOAT) 
+revert = gg.getResults(100) 
+gg.editAll(d[4],gg.TYPE_FLOAT) 
+gg.clearResults() 
+gg.sleep(500) 
+gg.setVisible(false)
+gg.setRanges(gg.REGION_CODE_APP)
+gg.searchNumber("0.1", gg.TYPE_FLOAT)
+revert = gg.getResults(100)
+gg.editAll(d[5],gg.TYPE_FLOAT)
+gg.clearResults() 
+gg.sleep(500)
+gg.setVisible(false)
+gg.processResume()
+gg.clearResults()
+gg.alert("BELI SEMULA ECU TUNING DAN FAST GEARBOX")
+end
+
+if menu == 3 then
+  gg.clearResults()
+  gg.clearList()
+  gg.sleep(300)
+  gg.toast(" SCRIPT EXIT ✅")
+  gg.sleep(200)
+  gg.toast("🇯  🇺 🇲 🇵 🇦  🇱 🇦 🇬 🇮💋 ")
+  os.exit()
+  end
+
+  
+if menu == nil then end
+menuk = - 1
+end 
+
+
+while(true) do
+if gg.isVisible(true) then
+menuk = 1
+gg.setVisible(false)
+end
+
+if menuk == 1 then start() end end
